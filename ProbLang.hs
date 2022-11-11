1a5c79656c1881e6b4dd5bf3e293892c585decdf
Julian Grove <julian.grove@gmail.com>
2022-11-11 00:39:36 -0500

    Standalone deriving for Field.

--- ProbLang.hs
+++ ProbLang.hs
@@ -22,8 +22,9 @@ import Control.Applicative
 -- dependencies on Probabilistic values explicitly; and thus we can
 -- reuse bigger parts of traces.
 newtype Probabilistic a = Prob {fromProb :: a}
-  deriving (Functor,Additive,Multiplicative,Group,AbelianAdditive,Show,Eq,Prelude.Num,Division,Field,Fractional,Floating)
+  deriving (Functor,Additive,Multiplicative,Group,AbelianAdditive,Show,Eq,Prelude.Num,Division,Fractional,Floating)
 deriving instance (Scalable (Probabilistic a) (Probabilistic a)) => Ring (Probabilistic a)
+deriving instance (Scalable (Probabilistic a) (Probabilistic a)) => Field (Probabilistic a)
 
 -- instance Ring a => Module (Probabilistic a) (Probabilistic a) where
   -- Prob a *^ Prob b = Prob (a * b)
